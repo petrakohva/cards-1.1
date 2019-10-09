58354a365dfbf04260e9400138e63fddfd8ce151
petrakohva <petra.kohva@haaga-helia.fi>
2019-10-09 10:06:19 +0300

    created this project

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+function connect_db() {
+ $servername = "localhost";
+ $username = "petra";
+ $password = "password";
+ $dbname = "petra";
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+ } else {
+ }
+ return $conn;
+}
+?>
+
